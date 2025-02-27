7984ad833c71665222940ad74bfe6c9da326ac6b
vinkabuki <vincent.kabacky@gmail.com>
2025-02-27 22:06:12 +0100

    asdf

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+function addition(a: number, b: number): number {
+    return a + b;
+}
+
+function subtraction(a: number, b: number): number {
+    return a - b;
+}
+
+function multiplication(a: number, b: number): number {
+    return a * b;
+}
+
+function division(a: number, b: number): number {
+    return a / b;
+}
