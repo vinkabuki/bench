2f82d227ac89e9ee6fc2165257c2080eac266aa1
vinkabuki <vincent.kabacky@gmail.com>
2025-03-27 13:24:01 +0100

    log

--- src/happy.py
+++ src/happy.py
@@ -2,6 +2,7 @@ def is_happy_number(n):
     seen = set()
     while n != 1 and n not in seen:
         seen.add(n)
+        print(n)
         n = sum(int(digit) ** 2 for digit in str(n))
     return n == 1
 
