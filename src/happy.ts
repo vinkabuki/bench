40189b06e8bc05f85b1bafcc559c89d9e709b097
vinkabuki <vincent.kabacky@gmail.com>
2025-04-10 21:34:15 +0200

    sraker

---
+++ src/happy.ts
@@ -0,0 +1,11 @@
+const isHappyNumber = (n: number): boolean => {
+    const seen = new Set<number>();
+    while (n !== 1 && !seen.has(n)) {
+        seen.add(n);
+        console.log(n);
+        n = n.toString().split('').reduce((acc, digit) => acc + Math.pow(parseInt(digit), 2), 0);
+    }
+    return n === 1;
+}
+
+isHappyNumber(19);
